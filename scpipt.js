850d40b04d633e6bc68938288c3fb54f420bd753
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-06-14 14:42:10 -0400

    flatten nested Array added

--- scpipt.js
+++ scpipt.js
@@ -12,3 +12,19 @@ try {
     console.log("Stack overflow error occurred!");
     console.log("Maximum call stack size reached:", counter);
 }
+
+
+// Part 2: Trampolines
+
+function flattenArray(arr) {
+    if (arr.length ===0) {
+        return [];
+    }
+
+    const [head, ...tail] = arr;
+    if (Array.isArray(head)) {
+        return flattenArray([...head, ...tail]);
+    }else {
+        return [head, ...flattenArray(tail)];
+    }
+}
