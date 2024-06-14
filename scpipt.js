b27be6fdf17378eaddf6c89e0be1721690e7793f
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-06-14 14:55:15 -0400

    The trampoline() function added

--- scpipt.js
+++ scpipt.js
@@ -17,14 +17,28 @@ try {
 // Part 2: Trampolines
 
 function flattenArray(arr) {
-    if (arr.length ===0) {
+    if (arr.length === 0) {
         return [];
     }
 
     const [head, ...tail] = arr;
     if (Array.isArray(head)) {
         return flattenArray([...head, ...tail]);
-    }else {
+    } else {
         return [head, ...flattenArray(tail)];
     }
 }
+
+function trampoline(fn) {
+    return function trampolined(...args) {
+        let result = fn(...args);
+        while (typeof result === 'function') {
+            result = result();
+        }
+        return result;
+    };
+}
+
+const trampolinedFlatten =trampoline(flattenArray);
+
+console.log(trampolinedFlatten([1,[2,[3,[4]]]]));
